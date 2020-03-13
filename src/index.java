016b08d1b1d7a035a5c2ff34af7543145990f50a
chenchuangzhi <61727791+chenchuangzhi@users.noreply.github.com>
2020-03-13 15:54:53 +0800

    Create index.java

---
+++ src/index.java
@@ -0,0 +1,259 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+public class index {
+    static     String    lujing = "file.c";   //文件的路径,未选取的时候默认为file.c文件
+    public static void main(String[] args) {
+        //设置主界面
+        JFrame mainFrame= new JFrame("WC.exe");
+        mainFrame.setSize(400,400);
+        mainFrame.setLayout(new GridLayout(3, 1));
+        mainFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent){
+                System.exit(0);
+            }
+        });
+        //设置按钮
+        JPanel controlPanel=new JPanel();
+        JButton zifu= new JButton("wc.exe -c(字符数)");
+
+        JButton danci = new JButton("wc.exe -w(单词数) ");
+        JButton hangshu = new JButton("wc.exe -l(行数)");
+        JButton a = new JButton("-a(代码行，注释行，空行)");
+        JButton wenjian= new JButton("选取文件(不使用该功能则默认统计项目内file.c)");
+        controlPanel.add(zifu);
+        controlPanel.add(hangshu);
+        controlPanel.add(danci);
+        controlPanel.add(a);
+        controlPanel.add(wenjian);
+        //将按钮放入主界面
+        mainFrame.add(controlPanel);
+        mainFrame.setVisible(true);
+
+        //统计文件的行数
+        hangshu.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int  k=line(lujing);
+                JOptionPane.showMessageDialog(null, "文件里面的行数为"+k);
+
+            }});
+
+        //统计文件的字符数
+        zifu.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int  k=zifu(lujing);
+                JOptionPane.showMessageDialog(null, "文件里面的字符数为"+k+"（包括空格不包括回车）");
+
+            }});
+
+        //统计文件的单词数
+        danci.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int  k=word(lujing);
+                JOptionPane.showMessageDialog(null, "文件里面的单词数为"+k);
+
+            }});
+
+ //统计文件里的空白行，注释行和代码行
+        a.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                a(lujing);
+
+
+            }});
+
+        wenjian.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser chooser = new JFileChooser(); //设置选择器
+                chooser.setMultiSelectionEnabled(true); //设为多选
+                int returnVal = chooser.showOpenDialog(wenjian); //是否打开文件选择框
+                System.out.println("returnVal="+returnVal);
+                if (returnVal == JFileChooser.APPROVE_OPTION) { //如果符合文件类型
+
+                   lujing =  chooser.getSelectedFile().getAbsolutePath();
+                }
+
+            }});
+
+    }
+
+
+
+    //统计文件的字符数
+    private static int zifu(String filename) {
+
+        File file=new File(filename);
+        BufferedReader reader=null;
+        int character=0;
+        try{
+            reader = new BufferedReader(new FileReader(file));
+            String tempchar;
+            while ((tempchar=reader.readLine()) != null) {
+                character +=  tempchar.length();
+            }
+
+            reader.close();
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, "不存在该文件");
+        }
+        return character;
+    }
+
+
+    //统计文件的单词数
+    public static int word(String fileName) {
+        File file = new File(fileName);
+        int word = 0;
+        BufferedReader reader = null;
+
+        try {
+
+            reader = new BufferedReader(new FileReader(file));
+
+            Pattern Pa =Pattern.compile("[a-zA-Z]+\\b");//判定为单词的正则表达式条件
+
+            String w=null;
+            while ((w = reader.readLine()) != null) {
+                Matcher Ma =Pa.matcher(w);
+                while(Ma.find()) //当找到单词时，单词数+1
+                    word++;
+            }
+            reader.close();
+        } catch (IOException k) {
+            k.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                }
+            }
+        }
+        // 返回单词数
+
+        return word;
+    }
+
+
+
+
+
+
+
+
+    //统计文档的行数
+    public static int line(String fileName) {
+        File file = new File(fileName);
+        int line = 0;
+        BufferedReader reader = null;
+        String tempString = null;
+        try {
+
+            reader = new BufferedReader(new FileReader(file));
+
+
+            // 一次读入一行，直到读入null为文件结束
+            while ((tempString = reader.readLine()) != null) {
+
+                line++;
+            }
+            reader.close();
+        } catch (IOException l) {
+            l.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                }
+            }
+        }
+        // 返回行数
+        return line;
+
+    }
+
+
+    //-a扩展程序，统计代码行数，注释行数与空白行数
+    public static void a(String fileName) {
+        File file = new File(fileName);
+        int blankcount = 0;   //空白行
+        int notescount = 0;   //注释行
+        int codecount = 0;    //代码行
+        BufferedReader reader = null;
+        String a = null;
+        int state = 0;//判断是否有注释标记
+        try {
+
+            reader = new BufferedReader(new FileReader(file));
+
+
+            // 一次读入一行，直到读入null为文件结束
+            while ((a = reader.readLine()) != null) {
+                a=remove(a,'{');
+                a=remove(a,'}');   //去除干扰判断的{和}符号
+                if(state == 0){        //注释没有标记时
+
+                    if(a.trim().indexOf("//") == 0 || a.trim().indexOf("/*") == 0)
+                        notescount++;
+
+                    else if(a.trim().length() > 1) 	codecount++;
+                    else blankcount++;
+                    if(a.contains("/*"))   state = 1;  //如果有/*，则注释标记
+
+                }
+                else
+                {
+
+                    if(a.contains("*/")) {    //取消注释标记
+                        state = 0;
+
+                    }
+                    notescount++;
+
+                }
+            }
+
+            reader.close();
+        } catch (IOException l) {
+            l.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                }
+            }
+        }
+        JOptionPane.showMessageDialog(null, "文件里面的空白行为"+blankcount+"\n代码行为"+codecount+"\n注释行为"+notescount);
+
+    }
+    //去除字符串中指定字符
+    public static String remove(String sourceString, char chElemData) {
+        String deleteString = "";
+        for (int i = 0; i < sourceString.length(); i++) {
+            if (sourceString.charAt(i) != chElemData) {
+                deleteString += sourceString.charAt(i);
+            }
+        }
+        return deleteString;
+    }
+
+
+}
